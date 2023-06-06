999f5ff678cc2a5ed926d1289a77acefa6f033df
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-06-06 04:49:09 -0300

    menu adjusts

--- scripts.js
+++ scripts.js
@@ -6,10 +6,12 @@ close.style.display = "none"
 
 hamburger.addEventListener("click", () => {
 	close.style.display = "block"
+	hamburger.style.display = "none"
 	sidebar.style.transform = "translateX(0%)"
 })
 
 close.addEventListener("click", () => {
 	close.style.display = "none"
+	hamburger.style.display = "block"
 	sidebar.style.transform = "translateX(100%)"
 })
