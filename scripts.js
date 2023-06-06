fd76f42c6285690c19496db15612c377a0036b25
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-06-06 04:40:12 -0300

    project done!

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let sidebar = document.querySelector(".sidebar")
+let hamburger = document.querySelector(".hamburger")
+let close = document.querySelector(".close")
+
+close.style.display = "none"
+
+hamburger.addEventListener("click", () => {
+	close.style.display = "block"
+	sidebar.style.transform = "translateX(0%)"
+})
+
+close.addEventListener("click", () => {
+	close.style.display = "none"
+	sidebar.style.transform = "translateX(100%)"
+})
